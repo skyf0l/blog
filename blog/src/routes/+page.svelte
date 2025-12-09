d36881003c93f7018ca731acce651ac3e4559e30
Skyf0l <59019720+skyf0l@users.noreply.github.com>
2025-12-09 14:28:13 +1100

    feat: sveltekit minimal + draft blog

---
+++ blog/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Meme from '$lib/assets/blog-on-aws-meme.png';
+</script>
+
+<svelte:head>
+	<title>Skyf0l</title>
+</svelte:head>
+
+<h1 class="text-4xl sm:text-5xl font-extrabold tracking-tight drop-shadow">Skyf0l</h1>
+
+<p class="text-sm">
+	Deployed on
+	<b>AWS Lambda</b>
+	using
+	<b>SvelteKit</b>.
+</p>
+
+<img src={Meme} alt="Blog on AWS meme" class="rounded-xl shadow-md" />
